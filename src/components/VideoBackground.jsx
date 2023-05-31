01590854cd43a181d9c186c4e40d8f4cb31094ec
Omar Fathy <91073589+OmarFathy22@users.noreply.github.com>
2023-05-31 08:30:19 +0300

    new

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,13 +1,24 @@
 /* eslint-disable react/prop-types */
 
-import Video from '../Video/video.mp4'
+import onlap from '../Video/video.mp4'
+import onMobile from '../Video/onMobile.mp4'
+import { useState , useEffect } from 'react';
 function VideoBackground({children}) {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  });
   return (
     <div>
       <video  width="300"
               height="300"
-               autoPlay muted loop id="myVideo" src={Video}
-               style={{ opacity:"0.5", position: "fixed", width: "100%", height: "100%", objectFit: "fill", zIndex: "-1" }}
+               autoPlay muted loop id="myVideo" src={width <= 900 ? onMobile : onlap}
+               style={{ opacity:"0.5", position: "fixed", width: "100%", height: "100%", objectFit: "cover", zIndex: "-1" }}
                >
         Your browser does not support HTML video.
       </video>
